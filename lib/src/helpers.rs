cb0bb54834b896715b564907443afc8bd5e10b78
Thitat Auareesuksakul <flux@thitat.net>
2024-03-13 22:58:16 +0900

    fix: make chrono auto-select precision when serializing ziso
    This is not ideal for DynamoDB, but on serde side it's hard-coded to be
    this way :(

--- lib/src/helpers.rs
+++ lib/src/helpers.rs
@@ -1,5 +1,5 @@
 use chrono::{DateTime, Utc};
 
 pub(crate) fn ziso_timestamp(dt: &DateTime<Utc>) -> String {
-    dt.to_rfc3339_opts(chrono::SecondsFormat::Nanos, true)
+    dt.to_rfc3339_opts(chrono::SecondsFormat::AutoSi, true)
 }
