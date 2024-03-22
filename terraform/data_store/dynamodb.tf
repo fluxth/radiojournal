d58070e4e20a462084402f0436b5c9978061a31b
Thitat Auareesuksakul <flux@thitat.net>
2024-03-22 23:46:29 +0900

    chore: fix pre-commit errors

--- terraform/data_store/dynamodb.tf
+++ terraform/data_store/dynamodb.tf
@@ -1,9 +1,9 @@
 resource "aws_dynamodb_table" "this" {
-  name           = var.name
-  billing_mode   = "PAY_PER_REQUEST"
+  name         = var.name
+  billing_mode = "PAY_PER_REQUEST"
 
-  hash_key       = "pk"
-  range_key      = "sk"
+  hash_key  = "pk"
+  range_key = "sk"
 
   attribute {
     name = "pk"
@@ -26,9 +26,9 @@ resource "aws_dynamodb_table" "this" {
   }
 
   global_secondary_index {
-    name               = "gsi1"
-    hash_key           = "gsi1pk"
-    range_key          = "gsi1sk"
-    projection_type    = "ALL"
+    name            = "gsi1"
+    hash_key        = "gsi1pk"
+    range_key       = "gsi1sk"
+    projection_type = "ALL"
   }
 }
