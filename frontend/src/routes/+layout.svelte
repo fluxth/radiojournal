3d26aa739255af7ca6c40b3ef4b8751c6ff08f0e
Thitat Auareesuksakul <flux@thitat.net>
2024-03-26 02:39:01 +0900

    feat(frontend): add nprogress when SPA naviation

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,14 @@
 <script>
+  import "nprogress/nprogress.css";
   import "../app.scss";
+
+  import NProgress from "nprogress";
+  import { navigating } from "$app/stores";
+
+  NProgress.configure({ minimum: 0.16, showSpinner: false });
+
+  $: if ($navigating) NProgress.start();
+  $: if (!$navigating) NProgress.done();
 </script>
 
 <div class="navbar bg-base-100 shadow-lg">
