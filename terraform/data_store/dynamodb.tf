09cd8ca69349483998877b9edac90d17e2cabb82
Thitat Auareesuksakul <flux@thitat.net>
2024-04-07 18:41:23 +0900

    terraform(data_store): remove gsi1 for now to save costs

--- terraform/data_store/dynamodb.tf
+++ terraform/data_store/dynamodb.tf
@@ -15,20 +15,20 @@ resource "aws_dynamodb_table" "this" {
     type = "S"
   }
 
-  attribute {
-    name = "gsi1pk"
-    type = "S"
-  }
+  #attribute {
+  #  name = "gsi1pk"
+  #  type = "S"
+  #}
 
-  attribute {
-    name = "gsi1sk"
-    type = "S"
-  }
+  #attribute {
+  #  name = "gsi1sk"
+  #  type = "S"
+  #}
 
-  global_secondary_index {
-    name            = "gsi1"
-    hash_key        = "gsi1pk"
-    range_key       = "gsi1sk"
-    projection_type = "ALL"
-  }
+  #global_secondary_index {
+  #  name            = "gsi1"
+  #  hash_key        = "gsi1pk"
+  #  range_key       = "gsi1sk"
+  #  projection_type = "ALL"
+  #}
 }
