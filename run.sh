5585e5779f6e1d72b1835c6a930a5358b8dd1b3c
Thitat Auareesuksakul <flux@thitat.net>
2024-05-07 00:37:04 +0900

    chore: introduce run.sh script

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+set -o allexport
+
+CWD="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+
+. "$CWD/.env.dev"
+
+if [[ "$1" == "logger" ]]; then
+  (
+    cd "$CWD/logger"
+    cargo lambda watch -p 9001
+  )
+elif [[ "$1" == "logger-invoke" ]]; then
+    cargo lambda invoke -p 9001 --data-ascii '{}' radiojournal-logger | jq
+elif [[ "$1" == "api" ]]; then
+  (
+    cd "$CWD/api"
+    cargo lambda watch
+  )
+fi
