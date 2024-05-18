69eceadb9c1e774aec8c02468b2ed78e55c2ae9f
Thitat Auareesuksakul <flux@thitat.net>
2024-05-18 10:36:49 +0900

    refactor(cli): move mock related functions into a separate file

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,147 +1,16 @@
-use std::sync::Arc;
+use radiojournal::crud::station::CRUDStation;
 
-use anyhow::Result;
-use tracing::info;
-
-use radiojournal::{
-    crud::{
-        station::{CRUDStation, Play as PlayTrait},
-        Context,
-    },
-    init, mock,
-    models::station::{AtimeStation, FetcherConfig, StationInDBCreate},
-};
-
-#[derive(Clone)]
-struct Play {
-    title: String,
-    artist: String,
-    is_song: bool,
-}
-
-impl PlayTrait for Play {
-    fn get_title(&self) -> &str {
-        self.title.as_str()
-    }
-
-    fn get_artist(&self) -> &str {
-        self.artist.as_str()
-    }
-
-    fn is_song(&self) -> bool {
-        self.is_song
-    }
-}
+mod mock;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt().compact().init();
 
-    let context = init::initialize()
+    let context = radiojournal::init::initialize()
         .await
         .expect("initialize radiojournal app");
 
     let crud_station = CRUDStation::new(context.clone());
 
-    mock(context, &crud_station).await;
-}
-
-async fn mock(context: Arc<Context>, crud_station: &CRUDStation) {
-    info!("Initializing DynamoDB table");
-    mock::table::delete_then_create_table(context)
-        .await
-        .unwrap();
-
-    let coolism = StationInDBCreate {
-        name: "coolism".to_string(),
-        location: None,
-        fetcher: Some(FetcherConfig::Coolism),
-    };
-
-    mock_station(crud_station, coolism).await.unwrap();
-
-    let efm = StationInDBCreate {
-        name: "efm".to_string(),
-        location: None,
-        fetcher: Some(FetcherConfig::Atime {
-            station: AtimeStation::EFM,
-        }),
-    };
-
-    mock_station(crud_station, efm).await.unwrap();
-
-    let greenwave = StationInDBCreate {
-        name: "greenwave".to_string(),
-        location: None,
-        fetcher: Some(FetcherConfig::Atime {
-            station: AtimeStation::Greenwave,
-        }),
-    };
-
-    mock_station(crud_station, greenwave).await.unwrap();
-
-    let chill = StationInDBCreate {
-        name: "chill".to_string(),
-        location: None,
-        fetcher: Some(FetcherConfig::Atime {
-            station: AtimeStation::Chill,
-        }),
-    };
-
-    mock_station(crud_station, chill).await.unwrap();
-
-    let z100 = StationInDBCreate {
-        name: "z100".to_string(),
-        location: Some("usa".to_string()),
-        fetcher: Some(FetcherConfig::Iheart {
-            slug: "whtz-fm".to_string(),
-        }),
-    };
-
-    mock_station(crud_station, z100).await.unwrap();
-
-    let kiis = StationInDBCreate {
-        name: "kiis".to_string(),
-        location: Some("usa".to_string()),
-        fetcher: Some(FetcherConfig::Iheart {
-            slug: "kiis-fm".to_string(),
-        }),
-    };
-
-    mock_station(crud_station, kiis).await.unwrap();
-}
-
-async fn mock_station(crud_station: &CRUDStation, station_create: StationInDBCreate) -> Result<()> {
-    info!(station_name = station_create.name, "Creating station");
-    let mut station = crud_station.create_station(station_create).await?;
-
-    info!(
-        station_name = station.name,
-        "Populating mock tracks and plays"
-    );
-
-    let play1 = Play {
-        title: "test song".to_string(),
-        artist: "some artist".to_string(),
-        is_song: true,
-    };
-
-    let play2 = Play {
-        title: "jingle".to_string(),
-        artist: format!("{} station id", station.name),
-        is_song: false,
-    };
-
-    let play3 = Play {
-        title: "another song".to_string(),
-        artist: "other artist".to_string(),
-        is_song: true,
-    };
-
-    crud_station.add_play(&mut station, play1).await?;
-    crud_station.add_play(&mut station, play2.clone()).await?;
-    crud_station.add_play(&mut station, play3).await?;
-    crud_station.add_play(&mut station, play2).await?;
-
-    Ok(())
+    mock::mock_database(context, &crud_station).await;
 }
