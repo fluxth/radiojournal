b77d5e98fb76683c2389031a15274c37a5d0d489
Thitat Auareesuksakul <flux@thitat.net>
2024-07-06 16:45:40 +0900

    refactor(lib): use provider pattern for CRUDPlay

---
+++ lib/src/crud/play/provider.rs
@@ -0,0 +1,56 @@
+use std::sync::Arc;
+
+use aws_sdk_dynamodb::error::SdkError;
+use aws_sdk_dynamodb::operation::query::{QueryError, QueryOutput};
+use aws_sdk_dynamodb::types::{AttributeValue, Select};
+use aws_smithy_runtime_api::client::orchestrator::HttpResponse;
+
+use crate::crud::Context;
+
+pub(super) struct DynamoDBProvider {
+    context: Arc<Context>,
+}
+
+pub(super) struct ExclusiveStartKey {
+    pub pk: String,
+    pub sk: String,
+}
+
+pub(super) struct QueryRangeInput {
+    pub pk: String,
+    pub start_sk: String,
+    pub end_sk: String,
+    pub limit: i32,
+    pub exclusive_start_key: Option<ExclusiveStartKey>,
+}
+
+impl DynamoDBProvider {
+    pub fn new(context: Arc<Context>) -> Self {
+        Self { context }
+    }
+
+    pub async fn query_range(
+        &self,
+        input: QueryRangeInput,
+    ) -> Result<QueryOutput, SdkError<QueryError, HttpResponse>> {
+        let mut query = self
+            .context
+            .db_client
+            .query()
+            .table_name(&self.context.db_table)
+            .key_condition_expression("pk = :pk AND sk BETWEEN :start_sk AND :end_sk")
+            .expression_attribute_values(":pk", AttributeValue::S(input.pk))
+            .expression_attribute_values(":start_sk", AttributeValue::S(input.start_sk))
+            .expression_attribute_values(":end_sk", AttributeValue::S(input.end_sk))
+            .select(Select::AllAttributes)
+            .limit(input.limit);
+
+        if let Some(exclusive_start_key) = input.exclusive_start_key {
+            query = query
+                .exclusive_start_key("pk", AttributeValue::S(exclusive_start_key.pk))
+                .exclusive_start_key("sk", AttributeValue::S(exclusive_start_key.sk));
+        }
+
+        query.send().await
+    }
+}
