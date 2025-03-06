37e09900a81cbd7c6855daaaf35645ebb4f9336f
Thitat Auareesuksakul <flux@thitat.net>
2025-03-07 00:34:48 +0900

    fix(api): drop unneeded api gateway related set env var call

--- api/src/main.rs
+++ api/src/main.rs
@@ -31,14 +31,6 @@ struct AppState {
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    // If you use API Gateway stages, the Rust Runtime will include the stage name
-    // as part of the path that your application receives.
-    // Setting the following environment variable, you can remove the stage from the path.
-    // This variable only applies to API Gateway stages,
-    // you can remove it if you don't use them.
-    // i.e with: `GET /test-stage/todo/id/123` without: `GET /todo/id/123`
-    std::env::set_var("AWS_LAMBDA_HTTP_IGNORE_STAGE_IN_PATH", "true");
-
     if init::use_localstack() {
         tracing_subscriber::fmt().compact().init();
     } else {
